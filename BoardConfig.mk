92b1e8a7b7e2016d9530c36b7d561b2b9ac212c0
Shas45558 <shas45558@gmail.com>
2026-08-01 19:04:00 +0600

    use prebuild kernal

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,20 @@
 #
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 ifeq ($(TARGET_PRODUCT),twrp_lancelot)
 DEVICE_PATH := device/xiaomi/lancelot
@@ -14,97 +24,27 @@ ifeq ($(TARGET_PRODUCT),twrp_merlinx)
 DEVICE_PATH := device/xiaomi/merlinx
 endif
 
+
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
+TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a75
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
-# APEX
-DEXPREOPT_GENERATE_APEX_IMAGE := true
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := mt6768
-TARGET_NO_BOOTLOADER := true
-
-# Display
-TARGET_SCREEN_DENSITY := 440
-
-# Boot Image
-BOARD_KERNEL_BASE := 0x40078000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_OFFSET := 0x00008000
-BOARD_RAMDISK_OFFSET := 0x07c08000
-BOARD_SECOND_OFFSET := 0xbff88000
-BOARD_KERNEL_TAGS_OFFSET := 0x0bc08000
-BOARD_DTB_OFFSET := 0x0bc08000
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_BOOT_HEADER_VERSION := 2
-BOARD_MKBOOTIMG_ARGS := --base $(BOARD_KERNEL_BASE)
-BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
-BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_SECOND_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive.usbconfigfs=true
-
-# Kernel
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/xiaomi/mt6768
-ifeq ($(TARGET_PRODUCT),twrp_lancelot)
-TARGET_KERNEL_CONFIG := lancelot_defconfig
-endif
-ifeq ($(TARGET_PRODUCT),twrp_merlinx)
-TARGET_KERNEL_CONFIG := merlin_defconfig
-endif
-TARGET_KERNEL_CLANG_COMPILE := true
-BOARD_KERNEL_SEPARATED_DTBO := true
-BOARD_INCLUDE_RECOVERY_DTBO := true
-BOARD_KERNEL_IMAGE_NAME := Image.gz
-
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
-TARGET_COPY_OUT_SYSTEM := system
-BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
-BOARD_SUPER_PARTITION_GROUPS := xiaomi_dynamic_partitions
-BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor product system_ext
-BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
-BOARD_USES_METADATA_PARTITION := true
-BOARD_ROOT_EXTRA_FOLDERS := metadata
-
-# Platform
-TARGET_BOARD_PLATFORM := mt6768
-
-# Recovery
-BOARD_INCLUDE_RECOVERY_DTBO := true
-TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
-# Security patch level
-VENDOR_SECURITY_PATCH := 2021-08-01
+# Assert
+TARGET_OTA_ASSERT_DEVICE := lancelot,galahad,merlin,merlinx
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
@@ -114,76 +54,100 @@ BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
-# Hack: prevent anti rollback
-PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 16.1.0
-
-# Recovery fstab
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
-
-# System-as-root
-BOARD_SUPPRESS_SECURE_ERASE := true
-
-# Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-
-# TWRP Configuration
-TW_CUSTOM_FLASHLIGHT := true
-TW_FLASHLIGHT_PATH := /sys/class/leds/flashlight/brightness
-TW_MAX_FLASHLIGHT_BRIGHTNESS := 2047
-TW_BRIGHTNESS_PATH := "/sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness"
-TW_MAX_BRIGHTNESS := 2047
-TW_DEFAULT_BRIGHTNESS := 205
-TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
-TW_INCLUDE_FASTBOOTD := true
-TW_INCLUDE_REPACKTOOLS := true
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
-TARGET_USES_MKE2FS := true
-TW_EXCLUDE_TWRPAPP := true
-TW_SKIP_COMPATIBILITY_CHECK := true
-TW_INCLUDE_REPACKTOOLS := true
-TW_INCLUDE_FB2PNG := true
-TW_INCLUDE_RESETPROP := true
-TW_LOAD_VENDOR_MODULES := true
-TW_FRAMERATE := 60
-TW_DEVICE_VERSION := lancelot/merlinx
-
-# OrangeFox 
-# Inherit the proprietary files
-OF_ENABLE_ALL_PARTITION_TOOLS := 1
-OF_PATCH_AVB20 := 1
-OF_DONT_PATCH_ENCRYPTED_DEVICE := 1
-OF_ENABLE_LPTOOLS := 1
-OF_FL_PATH1 := /sys/devices/platform/flashlights_led191/leds/flashlight
-# FRP
-OF_ENABLE_FRP_ADDON := 1
-# maximum permissible splash image size (in kilobytes); do *NOT* increase!
-OF_SPLASH_MAX_SIZE := 130
-ALLOW_MISSING_DEPENDENCIES := true
-LC_ALL := "C"
-TW_DEFAULT_LANGUAGE := "en"
-FOX_ENABLE_APP_MANAGER := 1
-FOX_USE_BASH_SHELL := 1
-FOX_USE_TAR_BINARY := 1
-FOX_USE_XZ_UTILS := 1
-FOX_USE_LZ4_BINARY := 1
-FOX_USE_ZSTD_BINARY := 1
-FOX_USE_BUSYBOX_BINARY := 1
-FOX_USE_DATE_BINARY := 1
-OF_ENABLE_ALL_PARTITION_TOOLS := 1
-OF_USE_LEGACY_BATTERY_SERVICES := 1
-OF_UNBIND_SDCARD_F2FS := 1
-OF_USE_DMCTL := 1
-
 #MIUI 12.xx A11 DECRYPTION
 BOARD_AVB_RECOVERY_ADD_HASH_FOOTER_ARGS += \
     --prop com.android.build.boot.os_version:$(PLATFORM_VERSION) \
     --prop com.android.build.boot.security_patch:$(PLATFORM_SECURITY_PATCH)
 
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_SYSTEM := system
+RECOVERY_SDCARD_ON_DATA := true
+TW_INCLUDE_NTFS_3G := true
 
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SUPER_PARTITION_SIZE := 6012043264
+BOARD_SUPER_PARTITION_GROUPS := main
+BOARD_MAIN_SIZE := 6012043264
+BOARD_MAIN_PARTITION_LIST := product vendor system system_ext
+BOARD_USES_METADATA_PARTITION := true
+
+# Kernel
+ifeq ($(TARGET_PRODUCT),twrp_lancelot)
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/lancelot/Image.gz
+endif
+
+ifeq ($(TARGET_PRODUCT),twrp_merlinx)
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/merlinx/Image.gz
+endif
+
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive=true
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_DTB_OFFSET := 0x0bc08000
+BOARD_RAMDISK_OFFSET := 0x07c08000
+BOARD_KERNEL_TAGS_OFFSET := 0x0bc08000
+
+# Image
+ifeq ($(TARGET_PRODUCT),twrp_lancelot)
+BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/lancelot/dtb
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/lancelot/dtbo.img
+endif
+
+ifeq ($(TARGET_PRODUCT),twrp_merlinx)
+BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/merlinx/dtb
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/merlinx/dtbo.img
+endif
+
+BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/dtb
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6768
+TARGET_BOOTLOADER_BOARD_NAME := mt6768
+
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
+
+# TWRP Configuration
+TW_THEME := portrait_hdpi
+TW_EXTRA_LANGUAGES := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_USE_TOOLBOX := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_Y_OFFSET := 80
+TW_H_OFFSET := -80
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true 
+TARGET_USES_MKE2FS := true
+TW_EXCLUDE_TWRPAPP := true
+TW_SKIP_COMPATIBILITY_CHECK := true
+
+# System as root
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# Selinux
+BOARD_SEPOLICY_VERS := 29.0.3
+SEPOLICY_IGNORE_NEVERALLOWS := true
+SELINUX_IGNORE_NEVERALLOWS := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
