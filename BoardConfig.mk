a2642956690ddb8aa6f1ae7cdbb46d7639755fb5
Arafattex <141769738+Shas45558@users.noreply.github.com>
2026-08-13 07:07:54 +0600

    Remove crypto configuration options
    Removed crypto-related configuration options from BoardConfig.mk.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,10 +184,4 @@ BOARD_AVB_RECOVERY_ADD_HASH_FOOTER_ARGS += \
     --prop com.android.build.boot.os_version:$(PLATFORM_VERSION) \
     --prop com.android.build.boot.security_patch:$(PLATFORM_SECURITY_PATCH)
 
-# Crypto
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_FBE := true
-TW_USE_FSCRYPT_POLICY := 2
-TW_INCLUDE_FBE_METADATA_DECRYPT := true
-TW_CRYPTO_USE_SYSTEM_VOLD := true
-
+l
