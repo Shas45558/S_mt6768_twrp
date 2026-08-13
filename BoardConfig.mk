0bd655d2fcb4548298e529f584689666241efb63
Shas45558 <shas45558@gmail.com>
2026-08-13 08:48:51 +0600

    lamo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,5 +183,3 @@ OF_USE_DMCTL := 1
 BOARD_AVB_RECOVERY_ADD_HASH_FOOTER_ARGS += \
     --prop com.android.build.boot.os_version:$(PLATFORM_VERSION) \
     --prop com.android.build.boot.security_patch:$(PLATFORM_SECURITY_PATCH)
-
-l
