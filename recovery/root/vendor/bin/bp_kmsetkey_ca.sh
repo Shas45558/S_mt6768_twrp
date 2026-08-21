2b0e7ec6f987c8ae641a0e2da08fabc4a0726629
Shas45558 <shas45558@gmail.com>
2026-08-21 12:47:34 +0600

    sec what happened

--- recovery/root/vendor/bin/bp_kmsetkey_ca.sh
+++ recovery/root/vendor/bin/bp_kmsetkey_ca.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
     /vendor/bin/bp_kmsetkey_ca -C
-    start keymaster-4-0
+    start keymaster-4-1
     setprop ro.fixup.ready true
 
--- recovery/root/vendor/bin/fix-decrypt-service.sh
+++ recovery/root/vendor/bin/fix-decrypt-service.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-stop keymaster-4-0
+stop keymaster-4-1
 stop keystore2
-start keymaster-4-0
+start keymaster-4-1
 start keystore2
 # umount /external_sd
 # rm -rf /external_sd
